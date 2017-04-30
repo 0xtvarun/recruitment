33957fefca46fbfdf20e9860c7d82e845261121a
0xtvarun <vtewari26@gmail.com>
2017-04-30 16:51:56 +0530

    Update socket.handshake.remoteAddress to socket.conn.remoteAddress

--- public/js/App.js
+++ public/js/App.js
@@ -1,5 +1,5 @@
-// var socket = io.connect("https://floating-hamlet-25041.herokuapp.com/");
-var socket = io.connect("http://localhost:3000");
+var socket = io.connect("https://floating-hamlet-25041.herokuapp.com/");
+// var socket = io.connect("http://localhost:3000");
 
 class App extends React.Component {
 
