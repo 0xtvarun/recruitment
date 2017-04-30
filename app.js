69e5c712fbc2108e9f5d75516012ee1d1812fd1b
0xtvarun <vtewari26@gmail.com>
2017-04-30 16:21:19 +0530

    add process.evn.PORT to work with Heroku

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var port = process.env.PORT || 3000;
+
 var ipaddr = [];
 
-server.listen(3000);
+server.listen(port);
 
 app.use(express.static(__dirname + '/public'));
 
